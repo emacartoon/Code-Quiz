d0cba75953a4c0ab035ec0b9d6900b5ef6a0e616
EmaCartoon <emacartoon@gmail.com>
2021-03-04 05:14:17 -0700

    More variables, styled and set up the html. Have first question displaying. Need to find how to move to next question and register the answer.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,26 +1,28 @@
-var $startBtn = document.querySelector("#start");
 var $start = document.querySelector("#start-prompt");
+var $startBtn = document.querySelector("#start-prompt button");
+var $questionPrompt = document.querySelector("#question-container");
+var $questionText = document.querySelector("#question-text");
+var $answers = document.querySelector("#answers");
 var $timer = document.querySelector("#timer");
 var time = 100;
 var highscores = [hs1, hs2, hs3, hs4, hs5];
-
+var $scores = document.querySelector("#highscores");
+var $hscore = document.querySelector("#hscore");
+var $explanation = document.querySelector("#explanation");
+var $gamestatus = document.querySelector("#game-status");
+var $luck = document.querySelector("#luck");
 
 // Set 5 questions and 5 answer arrays. each with 1 correct answer
 var questions = [
   {
     text: "Which company created JavaScript?",
-    answers: [
-      "IBM",
-      "Macintosh",
-      "Netscape",
-      "Google",
-    ],
-    correctAnswer: ["Netscape"],
+    answers: ["IBM", "Macintosh", "Netscape", "Google"],
+    correctAnswer: ["Netscape"]
   },
   {
     text: "Are Java and JavaScript the same language?",
     answers: ["Yes", "No"],
-    correctAnswer: ["No"],
+    correctAnswer: ["No"]
   },
   {
     text: "What does a concatenate function do?",
@@ -29,7 +31,7 @@ var questions = [
       "Display one string after another",
       "Convince cats to go eat",
     ],
-    correctAnswer: ["Display one string after another"],
+    correctAnswer: ["Display one string after another"]
   },
   {
     text: "Where does JavaScript go in best practices?",
@@ -40,7 +42,7 @@ var questions = [
       "Before </body>",
       "After </html>",
     ],
-    correctAnswer: ["In the <head>"],
+    correctAnswer: ["In the <head>"]
   },
   {
     text: "Where do you call down an external JavaScript file?",
@@ -51,7 +53,7 @@ var questions = [
       "Before </body>",
       "After </html>",
     ],
-    correctAnswer: ["Before </body>"],
+    correctAnswer: ["Before </body>"]
   },
 ];
 
@@ -68,34 +70,53 @@ $startBtn.addEventListener("click", function (e) {
   }, 1000);
   // hide start prompt
   $start.style.display = "none";
+  $explanation.style.display = "none";
+  $gamestatus.style.display = "none";
+  $scores.style.display = "none";
+  $questionPrompt.style.display = "all";
+  
   //display question
   $questionText.textContent = questions[0].text;
   // render answers
-
   questions[0].answers.forEach(function (item) {
-    var $answerBtn = document.createElement("button");
-    $answerBtn.textContent = item;
-    $answerBtn.addEventListener("click", handleAnswerClick);
-    $answers.append($answerBtn);
-  });
+    // create button
+    var $btn = document.createElement("button");
+    // set btn text
+    $btn.textContent = item;
+    // append to options div
+    $answers.append($btn);
+
+});
+
+// // if answer correct, q[i+1] and a[i+1]
+// if (questions[0].correctAnswer.textContent === onclick.answersbtn){
+//     find parent replace [n+1]
+// } 
+var $answersbtn = 
+$answersbtn.addEventListener("click", handleAnswersClick);
+function handleAnswersClick() {
+    var answord = document.getElementById($answersbtn).value;
+
+    console.log(answord);
+}
+
   // if answer incorrect, subtract time
 
-  // if answer correct, q[i+1] and a[i+1]
+  if (time === 0) {
+    //     // Stops execution of action at set interval
+    clearInterval(timeInterval);
+    //     // Calls function to display text in #winlose
+    displayMessage();
+}
 
-  //
+//      Calculate score
+// Check ig highscore - if yes, get initals
+// Save score + initials to localStorage
 
-  // if(time === 0) {
-  //     // Stops execution of action at set interval
-  //     clearInterval(timeInterval);
-  //     // Calls function to display text
-  //     displayMessage();
-  //      Calculate score
-
-  // Check ig highscore - if yes, get initals
-  // Save score + initials to localStorage
-
-  // If score > hs1, hs2, hs3, hs4, hs5, replace value in localstorage array
-  // Move hs_ to next rank based on which is replaced.
-  // localStorage.setItem("hs1", JSON.stringify(hs1));
-  // renderLastRegistered();
+// If score > hs1, hs2, hs3, hs4, hs5, replace value in localstorage array
+// Move hs_ to next rank based on which is replaced.
+// localStorage.setItem("hs1", JSON.stringify(hs1));
+// renderLastRegistered();
+// toggle #highscores
 });
+// Button $hscore displays #highscores
